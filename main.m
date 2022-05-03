7273464ef651e6cb1bf49d84f389df9d90fac91a
filip <filipry@student.chalmers.se>
2022-05-03 11:51:54 +0200

    updated optim process

--- main.m
+++ main.m
@@ -2,10 +2,36 @@ clearvars, clc
 Z = 20;
 e = 1.602176634e-19;
 
-% From lecture 5, rho0 = 0.17 mol/fm^2, a=1.2sqrt(A) fm, b=0.54 fm
-x0 = [0.1*e,4,0.6];
-options = optimset('PlotFcns',@optimplotfval);
-[Xhat,fval,exitflag,output]  = fminsearch(@(X) fun_to_minimize(X),x0, options);
+% From lecture 5, rho0 = 0.17nukleoner/fm^-3, a=1.2*A^1/3 fm, b=0.54 fm
+% This is equivalent to rho0 = 0.085*e fm^-3 , a=4.1 fm, b=0.54
+%x0 = [0.1*e,4,0.6];
+%x0 = [0.1*e,2,0.8];
+a = linspace(2.05,8.2,8);
+b = linspace(0.27,1.08,8);
+rho_0 = linspace(0.0425*e,0.17*e,8);
+
+%Used to generate optimdata.mat
+x = zeros(3,512);
+guesses = zeros(3,512);
+functionvalue = zeros(1,512);
+counter = 1;
+for i = 1:8
+    for j=1:8
+        for k=1:8
+            x0 = [rho_0(i),a(j),b(k)];
+            options = optimset('PlotFcns',@optimplotfval,'MaxIter',80);
+            [Xhat,fval,exitflag,output]  = fminsearch(@(X) fun_to_minimize(X),x0, options);
+            x(:,counter) = Xhat';
+            guesses(:,counter) = x0';
+            functionvalue(counter) = fval;
+            counter = counter + 1;
+        end
+    end
+end
+save('optimdata1.mat','x','guesses','functionvalue')
+
+%%
+Xhat = x(:,15)'
 
 fprintf("rho_0=%.2e, a=%.2e, b=%.2e\n",Xhat(1), Xhat(2), Xhat(3))
 
@@ -16,3 +42,6 @@ s = sum(integrand(R))*dr;
 rms = 4*pi/(Z*e)*s;
 fprintf("rms = %.2e\n", rms)
 
+%plot(R,rho(R,x0),"b")
+%hold on
+%plot(R,rho(R,Xhat),"r")
